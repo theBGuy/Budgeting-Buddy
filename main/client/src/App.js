0a513f779bc7b45455f15ebd03fe14256235fb59
l4lilul3lo <logankirk989@gmail.com>
2022-11-10 08:49:14 -0800

    add chart-js foundations

--- main/client/src/App.js
+++ main/client/src/App.js
@@ -1,15 +1,16 @@
 import React from "react";
- 
+
 // We use Route in order to define the different routes of our application
 import { Route, Routes } from "react-router-dom";
- 
+
 // We import all the components we need in our app
 import Navbar from "./components/navbar";
 import YearList from "./components/yearList";
 import Edit from "./components/edit";
 import CreateEnvelope from "./components/createEnvelope";
 import CreateYear from "./components/createYear";
- 
+import LineChart from "./components/chart-js/LineChart";
+
 const App = () => {
   return (
     <div>
@@ -19,9 +20,10 @@ const App = () => {
         {/* <Route path="/edit/:id" element={<Edit />} /> */}
         <Route path="/createEnvelope/:year/:month" element={<CreateEnvelope type="Envelope" />} />
         <Route path="/createYear" element={<CreateYear type="Year" />} />
+        <Route path="/lineChart" element={<LineChart />} />
       </Routes>
     </div>
   );
 };
- 
+
 export default App;
---
+++ main/client/src/components/chart-js/LineChart.js
@@ -0,0 +1,53 @@
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+function LineChart() {
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Line Chart',
+      },
+    },
+  };
+  const labels = ['January', 'February', 'March', 'April', 'May', 'June'];
+
+  const data = {
+    labels: labels,
+    datasets: [
+      {
+        label: 'My First dataset',
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: [0, 10, 5, 2, 20, 30, 45],
+      },
+    ],
+  };
+
+  return <Line options={options} data={data} />;
+}
+
+export default LineChart;
