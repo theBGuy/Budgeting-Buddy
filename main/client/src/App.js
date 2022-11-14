43fff488c94e9094d64cd7fd3db1eee8a9a983c6
l4lilul3lo <logankirk989@gmail.com>
2022-11-14 10:12:05 -0800

    update nav bar

--- main/client/src/App.js
+++ main/client/src/App.js
@@ -4,7 +4,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
  
 // We import all the components we need in our app
-import Navbar from "./components/nav/navbar";
+import NavBar from "./components/nav/NavBar";
 import CreateEnvelope from "./components/envelope/CreateEnvelope";
 import CreateYear from "./components/year/CreateYear";
 import EditYear from "./components/year/EditYear";
@@ -13,12 +13,15 @@ import Years from "./components/year/Years";
 const App = () => {
   return (
     <div>
-      <Navbar />
+      <NavBar />
       <Routes>
         <Route exact path="/" element={<Years />} />
         <Route path="/createYear" element={<CreateYear type="Year" />} />
         <Route path="/editYear/:year" element={<EditYear type="Year" />} />
-        <Route path="/:year/:month/createEnvelope" element={<CreateEnvelope />} />
+        <Route
+          path="/:year/:month/createEnvelope"
+          element={<CreateEnvelope />}
+        />
       </Routes>
     </div>
   );
