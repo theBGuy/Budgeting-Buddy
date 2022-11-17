e351d62e4ca57b2c593f7392d53edeef23c010be
theBGuy <60308670+theBGuy@users.noreply.github.com>
2022-11-17 18:30:34 -0500

    Update NavBar.js
    - add bootstrap.css

--- main/client/src/components/nav/NavBar.js
+++ main/client/src/components/nav/NavBar.js
@@ -3,6 +3,7 @@ import Toolbar from "@mui/material/Toolbar";
 import MenuSmall from "./MenuSmall";
 import NavLogo from "./NavLogo";
 import MenuMedium from "./MenuMedium";
+import "bootstrap/dist/css/bootstrap.css";
 
 function NavBar() {
   return (
